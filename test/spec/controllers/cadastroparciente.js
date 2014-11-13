7a8787dc2027f478bfebcefa3d326df4826bb0ee
Bruno Cau <Brunoalcau@users.noreply.github.com>
2014-11-13 17:05:52 -0300

    Testando servidor de integração

--- test/spec/controllers/cadastroparciente.js
+++ test/spec/controllers/cadastroparciente.js
@@ -1,18 +1,18 @@
-'use strict';
+// 'use strict';
 
-describe('Controller: CadastroparcienteCtrl', function () {
+// describe('Controller: CadastroparcienteCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('sascApp'));
+//   // load the controller's module
+//   beforeEach(module('sascApp'));
 
-  var CadastroparcienteCtrl,
-    scope;
+//   var CadastroparcienteCtrl,
+//     scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    CadastroparcienteCtrl = $controller('CadastroparcienteCtrl', {
-      $scope: scope
-    });
-  }));
-});
+//   // Initialize the controller and a mock scope
+//   beforeEach(inject(function ($controller, $rootScope) {
+//     scope = $rootScope.$new();
+//     CadastroparcienteCtrl = $controller('CadastroparcienteCtrl', {
+//       $scope: scope
+//     });
+//   }));
+// });
